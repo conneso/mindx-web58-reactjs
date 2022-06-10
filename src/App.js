1c1f81292b78b8dd4b1b92c10f49d71f21bbabca
Ho Huu Thanh <conneso.vn@gmail.com>
2022-06-10 22:37:42 +0700

    B11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Table from "./Table";
+import ArtistListComponent from './components/artists/artists-list.component'
+const users = [
+    {
+        email: "gowtham@outlook.com",
+        firstname: "gowtham",
+        lastname: "ss",
+        password: "outlook010"
+    },
+    {
+        email: "ss@ss.com",
+        firstname: "ss",
+        lastname: "ss",
+        password: "ss"
+    },
+    {
+        email: "gow@gow.com",
+        firstname: "gow",
+        lastname: "gow",
+        password: "gow"
+    },
+    {
+        email: "thanhhh@gow.com",
+        firstname: "Ho Huu",
+        lastname: "Thanh",
+        password: "wow"
+    }
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+// const App = () => <Table data={users} />;
+const App = () => <ArtistListComponent data={users} />;
 export default App;
